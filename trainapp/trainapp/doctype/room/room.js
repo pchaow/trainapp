98a50f0a0ea25e2d41a8f7a7668421541956ca6e
Chaow Porkaew <pchaowmobile@gmail.com>
2024-04-03 15:21:37 +0700

    show location only building

--- trainapp/trainapp/doctype/room/room.js
+++ trainapp/trainapp/doctype/room/room.js
@@ -1,8 +1,17 @@
 // Copyright (c) 2024, zezembly and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Room", {
-// 	refresh(frm) {
+frappe.ui.form.on("Room", {
+	refresh(frm) {
+        console.log(frm)
 
-// 	},
-// });
+        frm.set_query('location',function(){
+            return {
+                "filters" : {
+                    'location_type' : '1.อาคาร'
+                }
+            }
+        })
+
+	},
+});
