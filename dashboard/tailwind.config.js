6b2b0e4e596bc2646a83962bd789778132849167
Chaow Porkaew <pchaowmobile@gmail.com>
2024-04-10 14:25:42 +0700

    setup base structure

--- dashboard/tailwind.config.js
+++ dashboard/tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
+const {nextui} = require("@nextui-org/react");
+
+
 export default {
-  content: ["./src/**/*.{html,jsx,tsx,vue,js,ts}"],
+  content: ["./src/**/*.{html,jsx,tsx,vue,js,ts}",
+  "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [nextui()],
 }
 
