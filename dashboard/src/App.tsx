01726a51fc267e177978191fa0911d5d7fc4e24d
Chaow Porkaew <pchaowmobile@gmail.com>
2024-04-10 14:31:09 +0700

    update login

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import { FrappeProvider } from 'frappe-react-sdk'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import IndexPage from './pages/Index'
 function App() {
 
 
@@ -12,7 +13,8 @@ function App() {
 			>
 				<BrowserRouter basename={import.meta.env.VITE_BASE_PATH}>
 					<Routes>
-						<Route path='/' element={<div>INDEX</div>}  />
+						<Route path='/login' element={<LoginPage />} />
+						<Route path='/' element={<IndexPage />} />
 					</Routes>
 				</BrowserRouter>
 
