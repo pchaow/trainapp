2d1207133c3571fe6ace406159b747164f756796
Aoom088 <phawit088@gmail.com>
2024-04-10 15:00:18 +0700

    add

--- phawit_dashboard/src/App.tsx
+++ phawit_dashboard/src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import './App.css'
 import { FrappeProvider } from 'frappe-react-sdk'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import IndexPage from './pages/Index'
+import LoginPage from './pages/Login'
 function App() {
 
 
@@ -12,7 +14,8 @@ function App() {
 			>
 				<BrowserRouter basename={import.meta.env.VITE_BASE_PATH}>
 					<Routes>
-						<Route path='/' element={<div>INDEX</div>}  />
+						<Route path='/login' element={<LoginPage />} />
+						<Route path='/' element={<IndexPage />} />
 					</Routes>
 				</BrowserRouter>
 
