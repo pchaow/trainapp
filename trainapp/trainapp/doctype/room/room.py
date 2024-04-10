0702d944d37f0a9e817ea8799e201d192fff87c0
Chaow Porkaew <pchaowmobile@gmail.com>
2024-04-10 12:53:41 +0700

    update data

--- trainapp/trainapp/doctype/room/room.py
+++ trainapp/trainapp/doctype/room/room.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2024, zezembly and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
@@ -19,3 +19,54 @@ class Room(Document):
 	# end: auto-generated types
 
 	pass
+
+
+@frappe.whitelist()
+def create_room():
+	room = frappe.form_dict['room'] if 'room' in frappe.form_dict else None
+	if room :
+		room['doctype'] = "Room"
+		doc = frappe.get_doc(room)
+		doc.save()
+		return doc
+	
+	frappe.response['http_status_code'] = 400
+	return "room is missing"
+
+
+@frappe.whitelist()
+def get_rooms():
+	keyword = frappe.form_dict['keyword'] if 'keyword' in frappe.form_dict else ''
+	result = frappe.db.sql("""
+		select * from `tabRoom` where room_name like %(keyword)s
+						""",{ 'keyword' : f'%{keyword}%'},as_dict=True)
+	return result
+
+@frappe.whitelist(
+		#allow_guest=True
+		)
+def chaow() :
+	errors = {}
+	hasError = False
+	id = None
+	if 'id' in frappe.form_dict :
+		id = frappe.form_dict['id']
+	else :
+		errors['id'] = "ID is missing"
+		hasError = True
+
+	if hasError == False :
+		frappe.response['id'] = id
+		frappe.response['pet'] = {
+			'chaow' : { 'pet' : 'cat'}
+		}
+
+		return {
+			"name" : "chaow",
+			"age" : 19
+		}
+	else :
+		frappe.response['http_status_code'] = 400
+		frappe.response['hasError'] = hasError
+		frappe.response['errors'] = errors
+		return "ERROR"
