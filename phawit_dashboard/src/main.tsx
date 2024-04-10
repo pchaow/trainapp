8e68ec9dfef6e13ab7acbfaaa3d6c2fc439e9f7a
Aoom088 <phawit088@gmail.com>
2024-04-10 15:05:20 +0700

    dd

--- phawit_dashboard/src/main.tsx
+++ phawit_dashboard/src/main.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+<<<<<<< HEAD
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+=======
 import { NextUIProvider } from '@nextui-org/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -9,5 +15,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <NextUIProvider>
     <App />
     </NextUIProvider>
+>>>>>>> 2d1207133c3571fe6ace406159b747164f756796
   </React.StrictMode>,
 )
